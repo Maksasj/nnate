ffd6a164c6ff894578032c1219edce27a2335f39
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-06-06 15:23:45 +0300

    pushed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Flex, Button } from '@radix-ui/themes';
 import { useContext } from 'react';
 import { BlueberryContext } from './main';
+import { blueberryInstance } from 'blueb.js';
 
 function App() {
   const context = useContext(BlueberryContext);
@@ -11,6 +12,7 @@ function App() {
     return (
       <Flex>
         <Button onClick={() => {
+          console.log(blueberryInstance);
           // const heap = new Uint8Array((context.exports.memory as any).buffer, 0, 4096);
           // console.log(heap);
         }}>Hello</Button>
--- src/main.tsx
+++ src/main.tsx
@@ -12,9 +12,67 @@ import "./index.css";
 
 import "@radix-ui/themes/styles.css";
 
-import { BlueberryInstance, bluebInit } from "blueb.js";
+import { BlueberryInstance, Pointer, assert, bluebInit, bluebMapFloat32Array, blueberryInstance, concatenateArrays, make_environment, rand } from "blueb.js";
+import { PeachMatrix } from "blueb.js/dist/peach.map";
+import { internalPeachGetMatrixCols, internalPeachGetMatrixRows, internalPeachGetMatrixValues, internalPeachNewMatrix } from "blueb.js/dist/peach.h";
 
 export const BlueberryContext = createContext<BlueberryInstance | null>(null);
+
+export function peachMapMatrix(matrixPtr: Pointer) : PeachMatrix | null {
+  console.log("Mapping a matrix");
+  if(blueberryInstance === null) {
+    console.log("Isntance is null");
+
+    return null;
+  }
+
+  console.log(matrixPtr);
+  if(matrixPtr === null)
+      return null;
+
+  const rows = internalPeachGetMatrixRows(matrixPtr);
+  const cols = internalPeachGetMatrixCols(matrixPtr);
+  const valuePtr = internalPeachGetMatrixValues(matrixPtr);
+
+  console.log(rows);
+  console.log(cols);
+  console.log(valuePtr);
+
+  if(valuePtr === null)
+      return null;
+
+  const value = bluebMapFloat32Array(valuePtr, rows * cols);
+
+  if(value === null)
+      return null;
+
+  return {
+      rows: rows, 
+      cols: cols, 
+      value: value,
+      pointer: matrixPtr
+  };
+}
+
+export function paechNewMatrix(matrix: number[][]): PeachMatrix | null {
+  const rows = matrix[0].length;
+  const cols = matrix.length;
+
+  const ptr = internalPeachNewMatrix(rows, cols);
+  if(ptr === null)
+      return null;
+
+  console.log(ptr);
+
+  const map = peachMapMatrix(ptr);
+
+  if(map === null)
+      return null;
+
+  map.value.set(concatenateArrays<number>(matrix));
+  
+  return map;
+}
 
 const WasmContextProvider = (props: PropsWithChildren) => {
   const [instance, setInstance] = useState<BlueberryInstance | null>(null);
@@ -28,7 +86,18 @@ const WasmContextProvider = (props: PropsWithChildren) => {
 
     bluebInit().then((blueb) => {
       console.log(blueb);
+      console.log(blueberryInstance);
+
       setInstance(blueb);
+
+      const inputs = paechNewMatrix([
+        [0.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [1.0, 1.0]
+      ]);
+
+      // console.log(inputs);
     });
   }, []);
 
